dbea7bc5f0ee6020557ef3227bae796684efc391
Aristides Pascoal Queta <aristidesquetajr@gmail.com>
2023-08-06 19:18:21 +0100

    added: view of project

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/template/header/header.component';
+import { NavComponent } from './components/template/nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
