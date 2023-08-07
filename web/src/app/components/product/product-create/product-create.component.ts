74f51de77a7d5d28b5fc8fbaec942e83bb87f275
Aristides Pascoal Queta <aristidesquetajr@gmail.com>
2023-08-07 20:36:12 +0100

    added: model, service and change layout

--- web/src/app/components/product/product-create/product-create.component.ts
+++ web/src/app/components/product/product-create/product-create.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../product.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-create',
   templateUrl: './product-create.component.html',
   styleUrls: ['./product-create.component.scss']
 })
-export class ProductCreateComponent {
+export class ProductCreateComponent implements OnInit {
 
+  constructor(private productService: ProductService, private router: Router) { }
+
+  ngOnInit(): void {
+
+  }
+
+  createProduct(): void {
+
+  }
+
+  cancel(): void {
+    this.router.navigate(['/products'])
+  }
 }
